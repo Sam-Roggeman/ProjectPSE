2b69aa3d61529785c27ea8d3f0c9d01c32789c97
Sam-Roggeman <sam.roggeman@student.uantwerpen.be>
2021-02-26 14:11:24 +0100

    Cmakelist cpps

---
+++ src/ProjectMain.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TicTacToeMain.cpp
+// Author      : Serge Demeyer
+// Version     :
+// Copyright   : Project Software Engineering - BA1 Informatica - Serge Demeyer - University of Antwerp
+// Description : TicTactToe in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+}
---
+++ src/ProjectTests.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TicTacToeTests.cpp
+// Author      : Serge Demeyer
+// Version     :
+// Copyright   : Project Software Engineering - BA1 Informatica - Serge Demeyer - University of Antwerp
+// Description : TicTactToe in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
