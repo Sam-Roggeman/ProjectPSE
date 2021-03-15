00bcfeb5a4d8750430bac82931271a3f901ae34e
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2021-03-15 22:49:41 +0100

    Nog wat tests in Domain

--- src/ProjectDomainTests.cpp
+++ src/ProjectDomainTests.cpp
@@ -29,6 +29,7 @@ protected:
 /**
 Tests the default constructor.
 */
+//Tests voor de hub Constructor
 TEST_F(HubDomainTest, HubDefaultConstructor) {
     EXPECT_TRUE(hub.correctlyInitialized());
     // verify post-condition
@@ -37,21 +38,26 @@ TEST_F(HubDomainTest, HubDefaultConstructor) {
     EXPECT_EQ(0, hub.getTransport());
     EXPECT_EQ(0, hub.getLevering());
 
+    //Initialisering waardes
     hub.setLevering(500);
     hub.setInterval(6);
     hub.setTransport(50);
     hub.leveringToHub();
+
+    //Testen voor de gelijkheid
     EXPECT_EQ(500,hub.getLevering());
     EXPECT_EQ(500,hub.getAantalVaccins());
     EXPECT_EQ(50,hub.getTransport());
     EXPECT_EQ(6,hub.getInterval());
 
+    //Testen voor de niet gelijkheid
     EXPECT_NE(50,hub.getLevering());
     EXPECT_NE(500,hub.getTransport());
     EXPECT_NE(5,hub.getInterval());
     EXPECT_NE(1203,hub.getAantalVaccins());
 }
 
+//Tests voor de Vaccinatiecentra constructor
 TEST_F(HubDomainTest, CentrumDefaultConstructor ){
     EXPECT_TRUE(v.correctlyInitialized());
     EXPECT_EQ("", v.getNaamCentrum());
@@ -61,12 +67,15 @@ TEST_F(HubDomainTest, CentrumDefaultConstructor ){
     EXPECT_EQ(0, v.getAantalInwoners());
     EXPECT_EQ(0, v.getCapaciteit());
 
+    //Initialisering waardes
     v.setCapaciteit(1500);
     v.setNaamCentrum("Antwerpen");
     v.setAdresCentrum("vijfstraat 5");
     v.setAantalInwoners(500);
     v.setAantalVaccins(1300);
     v.setAantalGevaccineerden(1240);
+
+    //Gelijkheid testen
     EXPECT_EQ(1500,v.getCapaciteit());
     EXPECT_EQ("Antwerpen",v.getNaamCentrum());
     EXPECT_EQ("vijfstraat 5", v.getAdresCentrum());
@@ -75,16 +84,30 @@ TEST_F(HubDomainTest, CentrumDefaultConstructor ){
     EXPECT_EQ(1240,v.getAantalGevaccineerden());
 }
 
+
+//Tests voor de functie SubstractVaccins
 TEST_F(HubDomainTest, substractVaccins) {
+    //Initialisering waardes
     hub2.setLevering(1000);
     hub2.leveringToHub();
+
+    //Substract op de hub uitvoeren
     hub2.substractVaccins(0);
+
+    //Gelijkheid checken
     EXPECT_EQ(1000,hub2.getAantalVaccins());
+
+    //Substract op de hub uitvoeren
     hub2.substractVaccins(1000);
+
+    //Gelijkheid checken
     EXPECT_EQ(0,hub2.getAantalVaccins());
 
+    //Initialisering waardes
     hub2.leveringToHub();
     hub2.leveringToHub();
+
+    //Achtereenvolgens de substract uitvoeren en de gelijkheid bekijken
     hub2.substractVaccins(1000);
     EXPECT_EQ(1000,hub2.getAantalVaccins());
     hub2.substractVaccins(500);
@@ -97,65 +120,94 @@ TEST_F(HubDomainTest, substractVaccins) {
     EXPECT_EQ(0,hub2.getAantalVaccins());
 }
 
+//Tests voor de functie isLeveringsDag
 TEST_F(HubDomainTest, isLeveringsDag){
+    //Initialisering waardes
     hub2.setInterval(1);
     hub.setInterval(6);
+
+    //Kijken of de functie een True of False teruggeeft
     EXPECT_TRUE(hub2.isLeveringsDag(2));
     EXPECT_FALSE(hub2.isLeveringsDag(3));
     EXPECT_TRUE(hub.isLeveringsDag(7));
     EXPECT_FALSE(hub.isLeveringsDag(5));
     hub2.setInterval(0);
+
+    //Loopen en steeds de return waarde van de functie checken
+    //Deze zou altijd false moeten teruggeven
     while(hub2.getInterval() != 6){
         EXPECT_FALSE(hub.isLeveringsDag(8));
         hub2.setInterval(hub2.getInterval()+ 1);
     }
+
+    //Waarde initaliseren en vervolgens de return waarde van de functie bekijken
     hub2.setInterval(hub2.getInterval()+1);
     EXPECT_TRUE(hub2.isLeveringsDag(8));
 }
+
+//Tests voor de functie notDone
 TEST_F(HubDomainTest, notDone){
+    //Initialisering alle waardes
     vac.setAantalInwoners(1000);
     hub2.addcentrum(&vac);
     vac.setCapaciteit(1000);
+
+    //Testen op de return waarde van de functie
     EXPECT_TRUE(hub2.notDone());
     EXPECT_FALSE(hub.notDone());
+
+    //Initalisering van alle waardes
     vac.setAantalVaccins(1000);
     vac.vaccineren();
+
+    //Testen op de return waarde van de functie
     EXPECT_FALSE(hub2.notDone());
+    EXPECT_FALSE(hub.notDone());
 }
 
+//Tests voor de functie aantalOngevaccineerden
 TEST_F(HubDomainTest, aantalOngevaccineerden){
+    //Initialisering alle waardes
     vac.setAantalInwoners(1000);
     hub2.addcentrum(&vac);
     vac.setCapaciteit(1000);
     hub2.vaccineren();
+
+    //Testen op de return waarde van de functie
     EXPECT_EQ(0, hub.aantalOngevaccineerden());
     EXPECT_EQ(1000,hub2.aantalOngevaccineerden());
 }
+
+//Tests voor de functie addGevaccineerden
 TEST_F(HubDomainTest, addGevaccineerden){
+
+    //We voeren hier de functie uit
     vac.addGevaccineerden(500);
+
+    //We bekijken de waardes van het aantalgevaccineerden
     EXPECT_EQ(500,vac.getAantalGevaccineerden());
     EXPECT_EQ(0,v.getAantalGevaccineerden());
     EXPECT_NE(0,vac.getAantalGevaccineerden());
     EXPECT_NE(500,v.getAantalGevaccineerden());
+
+    //We voeren de functie weer 2 keer uit en kijken weer naar de output van getaantalgevaccineerden
     v.addGevaccineerden(5400);
     EXPECT_EQ(5400,v.getAantalGevaccineerden());
     vac.addGevaccineerden(1500);
     EXPECT_EQ(2000,vac.getAantalGevaccineerden());
 }
 
-TEST_F(HubDomainTest, addVacins){
+//Tests voor de functie addVaccins
+TEST_F(HubDomainTest, addVaccins){
+    //Initialisering alle waardes
     v.addVaccins(500);
     v.setAantalInwoners(1500);
     v.setCapaciteit(500);
+
+    //Returnwaarde vergelijken met verwachtte waarde
     EXPECT_EQ(500,v.getAantalVaccins());
     EXPECT_NE(0,v.getAantalVaccins());
     v.vaccineren();
     EXPECT_EQ(0,v.getAantalVaccins());
     EXPECT_NE(500,v.getAantalVaccins());
 }
-/*TODO
- * Vaccinatiecentrum:
-     * addGevaccineerden(int aantal_gevaccineerden);
-     * void addVaccins(int aantal);
-
-*/
