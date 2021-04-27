0b0ea480ffa3930519bf399600469e04fca52a24
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2021-04-27 16:50:14 +0200

    Yes

--- src/ProjectOutputTests.cpp
+++ src/ProjectOutputTests.cpp
@@ -77,3 +77,53 @@ TEST_F(HubOutputTest, outputSimulation){
     EXPECT_TRUE(FileCompare("./testOutput/testFiles/outputSimulation3.txt",
                             "./testOutput/testOutput/outputSimulation3.txt"));
 }
+
+TEST_F(HubOutputTest,Impressie){
+    hub.addcentrum(&v);
+    Simulation s = Simulation(&hub);
+    std::ofstream o;
+    v.setNaamCentrum("a");
+    v.setAdresCentrum("A");
+    v.setAantalInwoners(2000);
+    v.setCapaciteit(1000);
+    o.open("./testOutput/testOutput/ImpressieOutput1.txt");
+    s.impressie(o);
+    o.close();
+    EXPECT_TRUE(FileCompare("./testOutput/testOutput/ImpressieOutput1.txt",
+                            "./testOutput/testFiles/ImpressieOutput1.txt"));
+
+    VaccinType a = VaccinType();
+    a.setName("a");
+    std::map<std::string, VaccinType*> a2;
+    a2["a"] = &a;
+    v.setTypes(a2);
+    v.addVaccins(1500, "a");
+    v.vaccineren(0);
+    o.open("./testOutput/testOutput/ImpressieOutput2.txt");
+    s.impressie(o);
+    o.close();
+    EXPECT_TRUE(FileCompare("./testOutput/testOutput/ImpressieOutput2.txt",
+                            "./testOutput/testFiles/ImpressieOutput2.txt"));
+
+    Vaccinatiecentrum vaccinatiecentrum;
+    vaccinatiecentrum.setNaamCentrum("b");
+    vaccinatiecentrum.setAdresCentrum("b");
+    vaccinatiecentrum.setAantalInwoners(2000);
+    vaccinatiecentrum.setCapaciteit(1000);
+    hub.addcentrum(&vaccinatiecentrum);
+    VaccinType b = VaccinType();
+    b.setName("b");
+    b.sethernieuwing(1);
+    b.setGereserveerd(0);
+    b.setTransport(1000);
+    std::map<std::string, VaccinType*> a3;
+    a3["b"] = &b;
+    vaccinatiecentrum.setTypes(a3);
+    vaccinatiecentrum.addVaccins(1500, "b");
+    vaccinatiecentrum.vaccineren(0);
+    o.open("./testOutput/testOutput/ImpressieOutput3.txt");
+    s.impressie(o);
+    o.close();
+    EXPECT_TRUE(FileCompare("./testOutput/testOutput/ImpressieOutput3.txt",
+                            "./testOutput/testFiles/ImpressieOutput3.txt"));
+}
