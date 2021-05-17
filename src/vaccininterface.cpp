d7aa55fe9cd7121129850d6dbfd4d8ce423ea97f
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2021-05-17 21:44:18 +0200

    Yes

---
+++ src/vaccininterface.cpp
@@ -0,0 +1,20 @@
+#include "vaccininterface.h"
+#include "ui_vaccininterface.h"
+
+VaccinInterface::VaccinInterface(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::VaccinInterface)
+{
+    ui->setupUi(this);
+}
+
+VaccinInterface::~VaccinInterface()
+{
+    delete ui;
+}
+
+void VaccinInterface::on_pushButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Open Image"), "/home/jana", tr("Image Files ( *.xml)"));
+}
---
+++ src/vaccininterface.h
@@ -0,0 +1,25 @@
+#ifndef VACCININTERFACE_H
+#define VACCININTERFACE_H
+
+#include <QDialog>
+
+namespace Ui {
+class VaccinInterface;
+}
+
+class VaccinInterface : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit VaccinInterface(QWidget *parent = nullptr);
+    ~VaccinInterface();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::VaccinInterface *ui;
+};
+
+#endif // VACCININTERFACE_H
